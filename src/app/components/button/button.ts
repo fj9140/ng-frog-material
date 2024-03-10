c53a60599afd27ff2b34720ea8649edbf0c2982e
Mr Froge <fj9140@163.com>
2024-03-10 18:59:18 +0800

    dev: button

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector:`button[frog-button]`,
   templateUrl:'./button.html',
+  styleUrl:'./button.scss',
   standalone:true
 })
 export class FrogButton{}
