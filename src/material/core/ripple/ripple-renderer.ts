3a5219621e2e62a6f16033a16aa73eb8c81490ad
Mr Froge <fj9140@163.com>
2024-03-29 11:46:47 +0800

    dev

---
+++ src/material/core/ripple/ripple-renderer.ts
@@ -0,0 +1,8 @@
+
+export class RippleRenderer{
+  fadeInRipple(x:number,y:number){
+    const ripple=document.createElement('div');
+
+    ripple.style.left
+  }
+}
--- src/material/core/ripple/ripple.ts
+++ src/material/core/ripple/ripple.ts
@@ -1,10 +1,25 @@
-import { Component, Directive } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Directive,HostListener,HostBinding, Host } from "@angular/core";
+import { RippleRenderer } from "./ripple-renderer";
+
 
 @Component({
   selector:`fm-ripple`,
   exportAs:'fmRipple',
   styleUrls:['./ripple.scss','./variables.scss'],
   standalone:true,
-  templateUrl:'./ripple.html'
+  templateUrl:'./ripple.html',
+  imports:[CommonModule]
 })
-export class Ripple{}
+export class Ripple{
+
+  private _rippleRenderer:RippleRenderer;
+
+  constructor(){
+    this._rippleRenderer=new RippleRenderer()
+  }
+
+  launch(x:number,y:number){
+    return this._rippleRenderer.fadeInRipple(x,y);
+  }
+}
